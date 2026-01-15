7333f8278b68262ede59cd6c1c53682bb90863a2
Ccasabianca <107222996+Ccasabianca@users.noreply.github.com>
2026-01-15 15:27:07 +0100

    Ajout messages custom

--- app/Http/Requests/GiftRequest.php
+++ app/Http/Requests/GiftRequest.php
@@ -28,4 +28,17 @@ class GiftRequest extends FormRequest
             'price' => 'required|numeric|decimal:0,2',
         ];
     }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Le nom est obligatoire',
+            'name.min' => 'Le nom doit faire au moins :min caractères',
+            'name.max' => 'Le nom ne peut pas faire plus de :max caractères',
+            'url.url' => 'L\'URL n\'est pas valide',
+            'price.required' => 'Le prix est obligatoire',
+            'price.numeric' => 'Le prix doit être un nombre',
+            'price.decimal' => 'Le prix doit avoir au maximum 2 chiffres après la virgule',
+        ];
+    }
 }
